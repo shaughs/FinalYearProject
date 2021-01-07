14d7313dfa4e7eb4c37cd0ddc86b38b0203e8cfe
shaughs <73279192+shaughs@users.noreply.github.com>
2021-01-07 16:50:53 +0000

    Changes to internal loop

--- CanBus.c
+++ CanBus.c
@@ -15,22 +15,20 @@
 #include "peripherals.h"
 #include "MK64F12.h"
 
- flexcan_frame_t rxFrame;
- flexcan_frame_t txFrame;
+ flexcan_frame_t mbConfig;
+ flexcan_frame_t txFrame,rxFrame;
+ volatile bool rxComplete = false;
 
 /* CAN0_ORed_Message_buffer_IRQn interrupt handler */
 void CAN0_CAN_ORED_MB_IRQHANDLER(void) {
-	uint32_t mask;
-  /*  Place your code here */
-	mask = FLEXCAN_GetMbStatusFlags(CAN0,0x03);
-	printf("Status Flags: %x\n\r", mask);
-	FLEXCAN_ClearMbStatusFlags(CAN0, mask);
 
-	if(mask == 1){
-		FLEXCAN_ReadRxMb(CAN0, 0, &rxFrame);
-		printf("0x%02x\n\r", rxFrame.dataWord0);
-		printf("0x%02x\n\r", rxFrame.dataWord1);
+	printf("Message Buffer Status Flags: %x\n\r", FLEXCAN_GetMbStatusFlags(CAN0, 0x01));
+	if(FLEXCAN_GetMbStatusFlags(CAN0, 0x01) == 1){
+		FLEXCAN_ReadRxMb(CAN0, 0 , &rxFrame);
+		rxComplete = true;
 	}
+	FLEXCAN_ClearMbStatusFlags(CAN0, 0x01);
+
   /* Add for ARM errata 838869, affects Cortex-M4, Cortex-M4F
      Store immediate overlapping exception return operation might vector to incorrect interrupt. */
   #if defined __CORTEX_M && (__CORTEX_M == 4U)
@@ -48,33 +46,47 @@ int main(void) {
     /* Init FSL debug console. */
     BOARD_InitDebugConsole();
 #endif
+
     printf("Receive Frame Setup\n\r");
-    rxFrame.format = (uint8_t)kFLEXCAN_FrameFormatStandard;
-    rxFrame.type   = (uint8_t)kFLEXCAN_FrameTypeData;
+    mbConfig.format = (uint8_t)kFLEXCAN_FrameFormatStandard;
+    mbConfig.type   = (uint8_t)kFLEXCAN_FrameTypeData;
     //rxFrame.id     = (0x7DF);
-    rxFrame.id     = (0x7E8<<18);
-    FLEXCAN_SetRxMbConfig(CAN0, 0x7FF<<18 , &rxFrame, true);
+    mbConfig.id     = (0x7E8<<18);
+
 
     printf("Transmit Frame Setup\n\r");
     txFrame.format = (uint8_t)kFLEXCAN_FrameFormatStandard;
     txFrame.type   = (uint8_t)kFLEXCAN_FrameTypeData;
-    txFrame.id     = (0x7E8);
+    //txFrame.id     = (0x7E8);
+    txFrame.id     = (0x7DF);
     txFrame.length = 8;
 
     txFrame.dataWord0 = CAN_WORD0_DATA_BYTE_0(0x02) | CAN_WORD0_DATA_BYTE_1(0x01) | CAN_WORD0_DATA_BYTE_2(0x0D) |
-            CAN_WORD0_DATA_BYTE_3(0x55);
+            CAN_WORD0_DATA_BYTE_3(0x55) | CAN_WORD1_DATA_BYTE_4(0x55) | CAN_WORD1_DATA_BYTE_5(0x55) | CAN_WORD1_DATA_BYTE_6(0x55) |
+            CAN_WORD1_DATA_BYTE_7(0x55);
     txFrame.dataWord1 = CAN_WORD1_DATA_BYTE_4(0x55) | CAN_WORD1_DATA_BYTE_5(0x55) | CAN_WORD1_DATA_BYTE_6(0x55) |
             CAN_WORD1_DATA_BYTE_7(0x55);
 
+    printf("Send message\r\n");
+    printf("tx word0 = 0x%x\r\n", txFrame.dataWord0);
+    printf("tx word1 = 0x%x\r\n", txFrame.dataWord1);
+
     FLEXCAN_TransferSendBlocking(CAN0, 1, &txFrame);
 
-    printf("Complete\n\r");
+    /* Waiting for Message receive finish. */
+	while (!rxComplete)
+	{
+	}
+
+	printf("\r\nReceived message\r\n");
+
+	printf("rx word0 = 0x%x\r\n", rxFrame.dataWord0);
+	printf("rx word1 = 0x%x\r\n", rxFrame.dataWord1);
     printf("TX Frame ID: %x",txFrame.id);
-    printf("\n\rRX Frame ID: %x \n\r",rxFrame.id);
+    printf("\n\rRX Frame ID: %x \n\r",mbConfig.id);
 
     while(1){
     }
-
 
     return 0;
 }
