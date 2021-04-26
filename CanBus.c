5c2316b81eeba23350d55e7bf7da133de7a00d3a
shaughs <73279192+shaughs@users.noreply.github.com>
2021-04-26 12:51:54 +0100

    FreeRTOS Implemented in Canbus

--- CanBus.c
+++ CanBus.c
@@ -7,6 +7,7 @@
 #include "clock_config.h"
 #include "peripherals.h"
 #include "MK64F12.h"
+#include "queue.h"
 
  /*******************************************************************************
   * Definitions
@@ -21,10 +22,31 @@
   ******************************************************************************/
  uint8_t CAN_Tx_Frame(uint16_t ID, uint8_t *dataFrame, uint8_t msgBuffIndex);
  uint8_t rxBuffer[8];
- static volatile uint8_t frameReceived = 0;
+ static volatile uint8_t CANFrameReceived = 0;
  flexcan_frame_t txFrame, rxFrame;
 
- static uint8_t speedData = 0;
+uint8_t coolantTemp_Request[8] = {0x2,0x1,0x05,0x55,0x55,0x55,0x55,0x55};
+uint8_t RPM_Request[8] = {0x2,0x1,0x0C,0x55,0x55,0x55,0x55,0x55};
+uint8_t speed_Request[8] = {0x2,0x1,0x0D,0x55,0x55,0x55,0x55,0x55};
+uint8_t MAF_Request[8] = {0x2,0x1,0x10,0x55,0x55,0x55,0x55,0x55};
+uint8_t engineLoad_Request[8] = {0x2,0x1,0x04,0x55,0x55,0x55,0x55,0x55};
+
+Can_addresses[5] = {coolantTemp_Request, RPM_Request, speed_Request, MAF_Request, engineLoad_Request};
+
+static void canbus_task(void *pvParameters);
+static void display_task(void *pvParameters);
+QueueHandle_t canbusQueue = NULL;
+
+typedef struct {
+	uint8_t sourceID;
+	uint32_t dataField;
+}datastruct;
+
+#define coolTemp_ID 0
+#define RPM_ID 1
+#define speed_ID 2
+#define MAF_ID 3
+#define engineLoad_ID 4
 
  /*******************************************************************************
   * Code
@@ -34,12 +56,14 @@
 void CAN0_CAN_ORED_MB_IRQHANDLER(void) {
 	uint8_t x;
 
-	printf("Message Buffer Status Flags: %d\n\r", FLEXCAN_GetMbStatusFlags(CAN0, 0x01));
+	printf("Message Buffer Status Flag: %d\n\r", FLEXCAN_GetMbStatusFlags(CAN0, 0x01));
 
 	if(FLEXCAN_GetMbStatusFlags(CAN0, 0x01) == 1){
 		FLEXCAN_ClearMbStatusFlags(CAN0, 0x01);
 		FLEXCAN_ReadRxMb(CAN0, 0 , &rxFrame);
-		printf("Frame Received, ID: 0x%08x\n\r", (rxFrame.id & 0x1FFC0000)>>18);
+
+		printf("\rCAN Rx Frame Received, ID: 0x%08x\n\r", (rxFrame.id & 0x1FFC0000)>>18);
+
 		rxBuffer[0] = rxFrame.dataByte0;
 		rxBuffer[1] = rxFrame.dataByte1;
 		rxBuffer[2] = rxFrame.dataByte2;
@@ -48,11 +72,11 @@ void CAN0_CAN_ORED_MB_IRQHANDLER(void) {
 		rxBuffer[5] = rxFrame.dataByte5;
 		rxBuffer[6] = rxFrame.dataByte6;
 		rxBuffer[7] = rxFrame.dataByte7;
-		for(x=0;x<8;x++) {
+		for(x = 0; x < 8; x++) {
 			printf("%02x\t", rxBuffer[x]);
 		}
 		printf("\n\n\r");
-		frameReceived = 1;
+		CANFrameReceived = 1;
 }
 
   /* Add for ARM errata 838869, affects Cortex-M4, Cortex-M4F
@@ -71,12 +95,6 @@ const flexcan_rx_mb_config_t rxMbConfig = {
 
 int main(void) {
 
-	//uint8_t throttlePos_Request[8] = {0x2,0x1,0x11,0x55,0x55,0x55,0x55,0x55};
-	uint8_t RPM_Request[8] = {0x2,0x1,0x0C,0x55,0x55,0x55,0x55,0x55};
-	uint8_t speedRequest[8] = {0x2,0x1,0x0D,0x55,0x55,0x55,0x55,0x55};
-	uint8_t MAF_Request[8] = {0x2,0x1,0x10,0x55,0x55,0x55,0x55,0x55};
-	uint8_t engineLoad_Request[8] = {0x2,0x1,0x04,0x55,0x55,0x55,0x55,0x55};
-
   	/* Init board hardware. */
     BOARD_InitBootPins();
     BOARD_InitBootClocks();
@@ -87,90 +105,89 @@ int main(void) {
 #endif
 
     FLEXCAN_SetRxMbConfig(CAN0, 0 , &rxMbConfig, true);
-    // FLEXCAN_SetRxIndividualMask(CAN0, 0, 0X7FF<<18);
 
-    /* Setup Tx Message Buffer. */
-    //FLEXCAN_SetTxMbConfig(CAN0, 1, true);
+    canbusQueue = xQueueCreate(5, sizeof(datastruct));
+	vQueueAddToRegistry(canbusQueue, "Can Bus Queue");
+	xTaskCreate(canbus_task, "CanBus Task", 200, NULL, 3, NULL);
+	xTaskCreate(display_task, "Display Task", 150, NULL, 4, NULL);
+	vTaskStartScheduler();
 
-    /* Prepare Tx Frame for sending. */
-    txFrame.format = kFLEXCAN_FrameFormatStandard;
-    txFrame.type   = kFLEXCAN_FrameTypeData;
-    txFrame.id     = FLEXCAN_ID_STD(0x7DF);
-    txFrame.length = 8;
-
-    //txFrame.dataByte0 = 0x02;
-   // txFrame.dataByte1 = 0x01;
-   // txFrame.dataByte2 = 0x0D;
-   // txFrame.dataByte3 = 0x55;
-   // txFrame.dataByte4 = 0x55;
-   // txFrame.dataByte5 = 0x55;
-    //txFrame.dataByte6 = 0x55;
-    //txFrame.dataByte7 = 0x55;
-
-    //printf("Sending message\r\n");
- //   printf("tx word0 = 0x%x\r\n", txFrame.dataWord0);
-  //  printf("tx word1 = 0x%x\r\n", txFrame.dataWord1);
-
-    /* Send data through Tx Message Buffer using polling function. */
-  //  FLEXCAN_TransferSendBlocking(CAN0, 1, &txFrame);
-
-    while(1)
-    {
-    	CAN_Tx_Frame(0x7DF, speedRequest, 1);
-    	while(frameReceived == 0);
-    	frameReceived = 0;
-    	switch(rxBuffer[2]){
-    	case 0x0D:
-    		speedData = rxBuffer[3];
-    		printf("Speed: %dkm/h\n\n\r", rxBuffer[3]);
-    		printf("Speed Data: %dkm/h\n\n\r", speedData);
-    		break;
-    	case 0x0C:
-    		printf("RPM: %d\n\n\r", ((uint16_t)rxBuffer[3]*256+rxBuffer[4])/4); break;
-    	default: break;
-    	}
-    	CAN_Tx_Frame(0x7DF, RPM_Request, 1);
-    	while(frameReceived == 0);
-    	frameReceived = 0;
-    	switch(rxBuffer[2]){
-    	case 0x0D: printf("Speed: %dkm/h\n\n\r", rxBuffer[3]); break;
-    	case 0x0C: printf("RPM: %d\n\n\r", ((uint16_t)rxBuffer[3]*256+rxBuffer[4])/4); break;
-    	default: break;
-    	}
-    	CAN_Tx_Frame(0x7DF, MAF_Request, 1);
-		while(frameReceived == 0);
-		frameReceived = 0;
-		switch(rxBuffer[2]){
-		case 0x0D: printf("Speed: %dkm/h\n\n\r", rxBuffer[3]); break;
-		case 0x0C: printf("RPM: %d rpm\n\n\r", ((uint16_t)rxBuffer[3]*256+rxBuffer[4])/4); break;
-		case 0x10: printf("MAF: %d grams/sec\n\n\r", ((uint16_t)rxBuffer[3]*256+rxBuffer[4])/100); break;
-		default: break;
-		}
-	   	CAN_Tx_Frame(0x7DF, engineLoad_Request, 1);
-		while(frameReceived == 0);
-		frameReceived = 0;
-		switch(rxBuffer[2]){
-		case 0x0D: printf("Speed: %dkm/h\n\n\r", rxBuffer[3]); break;
-		case 0x0C: printf("RPM: %d rpm\n\n\r", ((uint16_t)rxBuffer[3]*256+rxBuffer[4])/4); break;
-		case 0x10: printf("MAF: %d grams/sec\n\n\r", ((uint16_t)rxBuffer[3]*256+rxBuffer[4])/100); break;
-		case 0x04: printf("Engine Load: %d %\n\n\r", ((100/255)*(rxBuffer[3]))); break;
-		default: break;
-		}
-	  	/*CAN_Tx_Frame(0x7DF, throttlePos_Request, 1);
-		while(frameReceived == 0);
-		frameReceived = 0;
-		switch(rxBuffer[2]){
-		case 0x0D: printf("Speed: %dkm/h\n\n\r", rxBuffer[3]); break;
-		case 0x0C: printf("RPM: %d rpm\n\n\r", ((uint16_t)rxBuffer[3]*256+rxBuffer[4])/4); break;
-		case 0x10: printf("MAF: %d grams/sec\n\n\r", ((uint16_t)rxBuffer[3]*256+rxBuffer[4])/100); break;
-		case 0x04: printf("Engine Load: %d \n\n\r", ((100/255)*(rxBuffer[3]))); break;
-		case 0x11: printf("Throttle Position: %d \n\n\r", ((100/255)*(rxBuffer[3]))); break;
-		default: break;
-		}*/
-    	for(uint32_t z=0; z<20000000; z++);
-    }
-    return 0 ;
+    while(1) {
+     }
+	return 0 ;
 }
+
+
+static void canbus_task(void *pvParameters) {
+	while(1){
+	 datastruct canbus_struct;
+	 uint8_t i = 0;
+
+		for(i = 0; i < 6; i++){
+
+			if(i == 5){
+				i = 0;
+			}
+
+			CAN_Tx_Frame(0x7DF, Can_addresses[i], 1);
+
+			while(CANFrameReceived == 0);
+			CANFrameReceived = 0;
+
+			switch(rxBuffer[2]){
+			case 0x05:
+				printf("Coolant Temperature: %d Degrees Celsius\n\n\r", (rxBuffer[3]));
+				canbus_struct.sourceID = 0;
+				canbus_struct.dataField = (rxBuffer[3]);
+				xQueueSend(canbusQueue, &canbus_struct, 0);
+				break;
+
+			case 0x0C:
+				printf("RPM: %d revolutions per minute\n\n\r", ((uint16_t)rxBuffer[3]*256+rxBuffer[4])/4);
+				canbus_struct.sourceID = 1;
+				canbus_struct.dataField = (((uint16_t)rxBuffer[3]*256+rxBuffer[4])/4);
+				xQueueSend(canbusQueue, &canbus_struct, 0);
+				break;
+
+			case 0x0D:
+				printf("Speed: %dkm/h\n\n\r", rxBuffer[3]);
+				canbus_struct.sourceID = 2;
+				canbus_struct.dataField = (rxBuffer[3]);
+				xQueueSend(canbusQueue, &canbus_struct, 0);
+				break;
+
+			case 0x10:
+				printf("MAF: %d grams/sec\n\n\r", ((uint16_t)rxBuffer[3]*256+rxBuffer[4])/100);
+				canbus_struct.sourceID = 3;
+				canbus_struct.dataField = (((uint16_t)rxBuffer[3]*256+rxBuffer[4])/100);
+				xQueueSend(canbusQueue, &canbus_struct, 0);
+				break;
+
+			case 0x04:
+				printf("Engine Load: %d %%\n\n\r", ((uint16_t)rxBuffer[3])*100/255);
+				canbus_struct.sourceID = 4;
+				canbus_struct.dataField = (((uint16_t)rxBuffer[3])*100/255);
+				xQueueSend(canbusQueue, &canbus_struct, 0);
+				break;
+
+			default:
+				break;
+			}
+			for(uint32_t z=0; z<2000000; z++);
+		}
+	}
+}
+
+static void display_task(void *pvParameters) {
+	datastruct display_struct;
+	printf("Starting Display Task\n\r");
+	while(1) {
+		if(xQueueReceive(canbusQueue, &display_struct, portMAX_DELAY) == pdTRUE){
+			printf("ID: %d Data: %d \n\r",display_struct.sourceID, display_struct.dataField);
+		}
+	}
+}
+
 
 uint8_t CAN_Tx_Frame(uint16_t ID, uint8_t *dataFrame, uint8_t msgBuffIndex) {
 	flexcan_frame_t txFrame;
@@ -178,6 +195,7 @@ uint8_t CAN_Tx_Frame(uint16_t ID, uint8_t *dataFrame, uint8_t msgBuffIndex) {
     txFrame.type   = (uint8_t)kFLEXCAN_FrameTypeData;
     txFrame.id     = FLEXCAN_ID_STD(ID);
     txFrame.length = 8;
+
     txFrame.dataByte0 = dataFrame[0];
     txFrame.dataByte1 = dataFrame[1];
     txFrame.dataByte2 = dataFrame[2];
@@ -186,9 +204,13 @@ uint8_t CAN_Tx_Frame(uint16_t ID, uint8_t *dataFrame, uint8_t msgBuffIndex) {
     txFrame.dataByte5 = dataFrame[5];
     txFrame.dataByte6 = dataFrame[6];
     txFrame.dataByte7 = dataFrame[7];
-    printf("Sending CAN Frame\n\r");
-    frameReceived = 0;
-    FLEXCAN_TransferSendBlocking(CAN0, msgBuffIndex, &txFrame);
-    printf("Frame Sent\n\r");
+
+    CANFrameReceived = 0;
+
+    printf("\rSending CAN Tx Frame\n\r");
+
+    FLEXCAN_TransferSendBlocking(CAN0, msgBuffIndex, &txFrame);\
+
+    printf("CAN TX Frame Sent\n\r");
     return 1;
 }
